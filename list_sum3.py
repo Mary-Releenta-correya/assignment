5fca83500fe2cd96fba57b7d0fde49abdaefeb53
mary-releenta-correya <releentarelu05@gmail.com>
2024-02-12 07:02:18 +0530

    assignmet3

---
+++ list_sum3.py
@@ -0,0 +1,9 @@
+list=[45,73,105,7,54,80]
+temp=[]
+for n in list:
+    sum=0
+    for digit in str(n):
+        sum=sum + int(digit)
+    temp.append(sum)
+print("sum of digits in",list,"is",str(temp))
+
